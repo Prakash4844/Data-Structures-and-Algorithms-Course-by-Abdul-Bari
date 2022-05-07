ce8ceef1d47fed6a8ae34674419c8f53f7269b9b
Prakash4844 <pk484442@gmail.com>
2022-05-07 21:15:45 +0530

    Updated some instructions in Chapter 3

--- 3. Required Setup for Programming/For_Linux.cpp
+++ 3. Required Setup for Programming/For_Linux.cpp
@@ -1,1 +1,3 @@
+//USE Visual Studio codium/Code or Compile through Terminal.
+
 //Follow This Documentation gcc in Linux: https://code.visualstudio.com/docs/cpp/config-linux
--- 3. Required Setup for Programming/For_Windows.cpp
+++ 3. Required Setup for Programming/For_Windows.cpp
@@ -1,3 +1,7 @@
+//NOTE: DO NOT USE TURBO C++ OR DEV C++, THEY ARE REALLY OUTDATED.
+
+//USE Visual Studio code or Visual Studio Community or Compile through Terminal.
+
 //Follow This Documentation gcc in Windows: https://code.visualstudio.com/docs/cpp/config-mingw
 
 //or
